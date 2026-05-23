bd4e1560893b09a4b599c5cd30bdc876b5355a1c
hritikdangol <hritikdangol@gmail.com>
2026-05-23 17:43:34 +0545

    Add files via upload

---
+++ Assignment/easy/Evenodd.java
@@ -0,0 +1,13 @@
+public class Evenodd {
+    public static void main (String[] args) {
+        int n = 3;
+        if(n%2==0)
+        {
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+        
+    }  
+}
