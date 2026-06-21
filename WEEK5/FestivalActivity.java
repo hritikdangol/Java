846194d6070edd4ccf27949cb4a2371f4502bae7
hritikdangol <hritikdangol@gmail.com>
2026-06-21 19:47:10 +0545

    git commit -m

---
+++ WEEK5/FestivalActivity.java
@@ -0,0 +1,20 @@
+package WEEK5;
+class FestivalplanningException extends Exception{
+    FestivalplanningException(String message){
+        super(message);
+    }
+}
+public abstract class FestivalActivity{
+    String activityName;
+    double estimatedCost;
+    FestivalActivity(String an,double ec){
+        this.activityName=an;
+        this.estimatedCost=ec;
+    }
+   public abstract void planActivity() throws FestivalplanningException;
+   void display(){
+    System.out.println("Activity Name:"+activityName);
+    System.out.println("Estimated COst:"+estimatedCost);
+   }
+
+}
